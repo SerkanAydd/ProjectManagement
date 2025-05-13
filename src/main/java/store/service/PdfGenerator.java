fddb8b108e44c766498c9b601a46f0ee31d4d60e
Serkan Aydoğdu <serkanad40@gmail.com>
2025-05-13 22:49:14 +0300

    writing on template for diploma

--- src/main/java/store/service/PdfGenerator.java
+++ src/main/java/store/service/PdfGenerator.java
@@ -1,12 +1,16 @@
 package store.service;
 
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import store.entity.Student;
 
+import java.io.InputStream;
 import java.util.List;
 
 public class PdfGenerator {
@@ -16,49 +20,69 @@ public class PdfGenerator {
         for (Student student : students) {
             String outputPath_ =  "diplomas/" + student.getStudentid() + ".pdf";
 
-            System.out.println(outputPath_);
-
             try {
-                PageSize customSize = new PageSize(842, 500);
                 PdfWriter writer = new PdfWriter(outputPath_);
-                PdfDocument pdf = new PdfDocument(writer);
-                pdf.setDefaultPageSize(customSize);
-                Document document = new Document(pdf);
+                PdfDocument pdfDoc = new PdfDocument(writer);
+                PageSize customSize = new PageSize(600, 425);
+                pdfDoc.setDefaultPageSize(customSize);
+                Document document = new Document(pdfDoc);
 
-                Paragraph p = new Paragraph("Türkiye Cumhuriyeti");
-                p.setFirstLineIndent(310);
-                document.add(p);
+                InputStream imageStream = PdfGenerator.class.getClassLoader().getResourceAsStream("diploma.png");
+                ImageData imageData = ImageDataFactory.create(imageStream.readAllBytes());
+                Image background = new Image(imageData);
+                background.scaleToFit(PageSize.A4.getWidth(), PageSize.A4.getHeight());
+                background.setFixedPosition(0, 0);
+                document.add(background);
 
-                Paragraph p1 = new Paragraph("REPUBLIC OF TURKIYE");
-                p1.setFirstLineIndent(300);
-                document.add(p1);
+                String student_name = student.getName();
+                int name_length = student_name.length();
+                int name_halfLength = name_length / 2;
+                Paragraph name = new Paragraph(student_name).setFontSize(16);
+                name.setFixedPosition(260 - name_halfLength, 285, 400);  // adjust X, Y, Width
+                document.add(name);
 
-                Paragraph p2 = new Paragraph("IZMIR YUKSEK TEKNOLOJI ENSTITUSU");
-                p2.setFirstLineIndent(250);
-                document.add(p2);
+                String faculty_name_tr = student.getFaculty();
+                if (faculty_name_tr.equals("Engineering")) {
+                    faculty_name_tr = "Muhendislik";
+                } else if (faculty_name_tr.equals("Science")) {
+                    faculty_name_tr = "Bilim";
+                } else if (faculty_name_tr.equals("Architect")) {
+                    faculty_name_tr = "Mimarlik";
+                } else {
 
-                Paragraph p3 = new Paragraph("IZMIR INSTITUTE OF TECHNOLOGY");
-                p3.setFirstLineIndent(255);
-                document.add(p3);
+                }
 
-                Paragraph p4 = new Paragraph(student.getName());
-                p4.setFirstLineIndent(320);
-                document.add(p4);
+                Paragraph faculty = new Paragraph(faculty_name_tr).setFontSize(9);
+                faculty.setFixedPosition(248, 242, 400);  // change coordinates accordingly
+                document.add(faculty);
 
-                Paragraph p5 = new Paragraph(student.getFaculty());
-                p5.setFirstLineIndent(300);
-                document.add(p4);
+                Paragraph faculty_eng = new Paragraph(student.getFaculty()).setFontSize(9);
+                faculty_eng.setFixedPosition(267, 220, 400);  // change coordinates accordingly
+                document.add(faculty_eng);
 
-                Paragraph p6 = new Paragraph(student.getDepartment());
-                p6.setFirstLineIndent(320);
-                document.add(p6);
+                Paragraph faculty_tr = new Paragraph(faculty_name_tr).setFontSize(15);
+                faculty_tr.setFixedPosition(248, 198, 400);  // change coordinates accordingly
+                document.add(faculty_tr);
 
-                /*
-                document.add(new Paragraph("ID: " + student.getStudentid()));
-                document.add(new Paragraph("Name: " + student.getName()));
-                document.add(new Paragraph("Faculty: " + student.getFaculty()));
-                document.add(new Paragraph("Department: " + student.getDepartment()));
-                */
+                Paragraph faculty_eng2 = new Paragraph(student.getFaculty()).setFontSize(7);
+                faculty_eng2.setFixedPosition(255, 178, 400);  // change coordinates accordingly
+                document.add(faculty_eng2);
+
+                String title_ = student.getFaculty();
+                if (title_.equals("Engineering")) {
+                    title_ = "Muhendis";
+                } else if (title_.equals("Science")) {
+                    title_ = "Bilim Insani";
+                } else if (title_.equals("Architect")) {
+                    title_ = "Mimar";
+                } else {
+
+                }
+
+                Paragraph title = new Paragraph(title_).setFontSize(15);
+                title.setFixedPosition(190, 150, 400);  // change coordinates accordingly
+                document.add(title);
+
                 document.close();
             } catch (Exception e) {
                 e.printStackTrace();
