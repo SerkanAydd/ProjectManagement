9973d3c635604cf7c9f87793300bc76500c452c6
N1GHTR0 <ahmet.yilmaznght@gmail.com>
2025-05-19 16:02:12 +0300

    Add new controllers, entities, repositories and services for student views

---
+++ src/main/java/store/service/StudentService.java
@@ -0,0 +1,70 @@
+package store.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import store.entity.Student;
+import store.repository.StudentRepository;
+
+
+@Service
+public class StudentService 
+{
+    @Autowired
+    private StudentRepository stdRepo;
+
+    public List<Student> getRankedStudentListByDepartment(String departmentName)
+    {
+        List<Student> rankStudents = stdRepo.getRankedStudentListByDepartment(departmentName);
+
+        if (rankStudents == null || rankStudents.isEmpty())
+        {
+            throw new RuntimeException("No students found for ranking in this department, department name: >" + departmentName);
+        }
+        return (rankStudents);
+    }
+
+    public List<Student> getRankedStudentListByFaculty(String facultyName)
+    {
+        List<Student> rankStudents = stdRepo.getRankedStudentListByFaculty(facultyName);
+
+        if (rankStudents == null || rankStudents.isEmpty())
+        {
+            throw new RuntimeException("No students found for ranking in this faculty, faculty name: >" + facultyName);
+        }
+        return (rankStudents);
+    }
+
+    public List<Student> getRankedStudentList() 
+    {
+        List<Student> rankStudents = stdRepo.getRankedStudentList();
+
+        if (rankStudents == null || rankStudents.isEmpty())
+        {
+            throw new RuntimeException("No students found for ranking");
+        }
+        return (rankStudents);
+    }
+
+    public List<Student> getStudentRankingListByDepartment(String departmentName)
+    {
+        List<Student> students = stdRepo.getStudentListByDeparment(departmentName);
+    
+        if (students == null || students.isEmpty()) 
+        {
+            throw new RuntimeException("No students found for department: >" + departmentName);
+        }
+        return (students);
+    }
+}
